d442bd258503f35000c5a6bbfbeaa4522013fbb8
LlamaUnicorn <holidaynight@gmail.com>
2022-03-07 19:45:41 +0300

    FPW: Finished C4

---
+++ C_OOP/C4/4.8.5.py
@@ -0,0 +1,14 @@
+array = [2, 3, 1, 4, 6, 5, 9, 8, 7]
+cnt = 0
+for i in range(1, len(array)):
+    x = array[i]
+    idx = i
+    while idx > 0:
+        cnt += 1
+        if not(array[idx-1] > x):
+            break
+        array[idx] = array[idx-1]
+        idx -= 1
+    array[idx] = x
+
+print(cnt)
