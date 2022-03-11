29ad1b98296b216797038b4c6761324d7d571154
LlamaUnicorn <holidaynight@gmail.com>
2022-03-11 08:10:01 +0300

    FPW: C5.6 added to do list

--- C_OOP/C5/Conversion_bot/app.py
+++ C_OOP/C5/Conversion_bot/app.py
@@ -1,15 +1,3 @@
-# Botfather - new bot
-# Create venv: python -m venv venv
-# Activate venv for Win: venv\scripts\activate
-# If successful terminal shows (venv) C:\Users...
-# pip3 install pytelegrambotapi
-# pip3 install requests
-# cryptocompare.com / API / Get your free api key /free personal / Register account
-# visit API/Documentation page
-# import json
-# import pytelegrambotapi
-# import requests
-
 # config.py:
 
 # TOKEN = ''
@@ -21,16 +9,32 @@
 #     'доллар': 'USD',
 # }
 
+# TODO: Бот возвращает цену на определённое количество валюты (евро, доллар или рубль).
+# TODO: При написании бота необходимо использовать библиотеку pytelegrambotapi.
+# TODO: Человек должен отправить сообщение боту в виде <имя валюты, цену которой он хочет узнать> <имя валюты, в которой надо узнать цену первой валюты> <количество первой валюты>.
+# TODO: При вводе команды /start или /help пользователю выводятся инструкции по применению бота.
+# TODO: При вводе команды /values должна выводиться информация о всех доступных валютах в читаемом виде.
+# TODO: Для получения курса валют необходимо использовать API и отправлять к нему запросы с помощью библиотеки Requests.
+# TODO: Для парсинга полученных ответов использовать библиотеку JSON.
+# TODO: При ошибке пользователя (например, введена неправильная или несуществующая валюта или неправильно введено число) вызывать собственно написанное исключение APIException с текстом пояснения ошибки.
+# TODO: Текст любой ошибки с указанием типа ошибки должен отправляться пользователю в сообщения.
+# TODO: Для отправки запросов к API описать класс со статическим методом get_price(), который принимает три аргумента и возвращает нужную сумму в валюте:
+# TODO: - имя валюты, цену на которую надо узнать, — base;
+# TODO: - имя валюты, цену в которой надо узнать, — quote;
+# TODO: - количество переводимой валюты — amount.
+# TODO: Токен Telegram-бота хранить в специальном конфиге (можно использовать .py файл).
+# TODO: Все классы спрятать в файле extensions.py.
+
+# TODO: Перевести пользовательский ввод в lowercase
+# TODO: Найти падежи в практикуме
+# TODO: Добавить правильные падежи в ответы: 25 доллар_ов = 2566 рублей
+# TODO: Добавить погоду
+
 import telebot
 from config import keys, TOKEN
 from utils import ConvertionException, CryptoConverter
 
 bot = telebot.TeleBot(TOKEN)
-
-
-# @bot.message_handler()
-# def echo_test(message: telebot.types.Message):
-#     bot.send_message(message.chat.id, 'hello')
 
 
 @bot.message_handler(commands=['start', 'help'])
